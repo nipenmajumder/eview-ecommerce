1aabff6f7016d3dbfceb7c327d04ffb787000c99
komol-chandra <kiri2kaofficial@gmail.com>
2022-02-16 17:47:14 +0600

    22 offer

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -106,6 +106,84 @@
     </section>
 </div>
 <!--=======================  New collection products start ==============================  -->
+@if($today==16)
+@if($alltweentyproduct->count()> 0)
+
+<div class="title1 section-t-space">
+    <h2 class="title-inner1">22 Offer Is Here</h2>
+</div>
+<section class="section-b-space pt-0 ratio_asos">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="product-5 product-m no-arrow">
+                    @foreach ($alltweentyproduct as $product)
+                    @php
+                    $discounted_price = ($product->product_price)-($product->product_price*(11/100));
+                    @endphp
+                    <form id="cartsection-{{ $product->id }}">
+                        <input type="hidden" name="id" value="{{$product->id}}">
+                        <input type="hidden" name="name" value="{{$product->product_name}}">
+                        <input type="hidden" name="product_sku" value="{{$product->product_sku}}">
+                        <input type="hidden" name="image" value="{{$product->image}}">
+                        <input type="hidden" name="shop_id" value="{{$product->shop_id}}">
+                        <input type="hidden" name="price" value="{{$discounted_price}}">
+                        <input type="hidden" name="product_main_price" value="{{$product->product_price}}">
+                        <input type="hidden" name="product_quantity" value="1">
+                        <div class="product-box">
+                            <div class="img-wrapper">
+                                <div class="lable-block">
+                                    @if($product->offer!=null)
+                                    <span class="lable3">{{ $product->offer }}</span>
+                                    @endif
+                                    <span class="lable4">on sale</span>
+                                </div>
+                                <div class="front">
+                                    <a href="{{url('/products/'.$product->product_slug.'/'.$product->id)}}">
+                                        <img src="{{ asset('uploads/products/'.$product->image) }}"
+                                            class="img-fluid blur-up lazyload bg-img" alt="">
+                                    </a>
+                                </div>
+                                <div class="cart-info cart-wrap">
+                                    <button id="{{$product->id}}" type="button" onclick="addtocart(this)"
+                                        title="Add to cart"><i class="ti-shopping-cart"></i></button>
+
+                                    <a id="{{$product->id}}" href="javascript:void(0)" onclick="addtowishlist(this)"
+                                        title="Add to Wishlist"><i class="ti-heart" aria-hidden="true"></i></a>
+                                    <a class="productdetails" data-id="{{ $product->id }}" data-bs-toggle="modal"
+                                        data-bs-target="#quick-view" title="Quick View"><i class="ti-search"
+                                            aria-hidden="true"></i></a>
+                                    <a href="" title="Compare"><i class="ti-reload" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                            <div class="product-detail">
+                                {{-- <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
+                                        class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                                </div> --}}
+                                <a href="{{url('/products/'.$product->product_slug.'/'.$product->id)}}">
+                                    <h6>{{ $product->product_name }}</h6>
+                                </a>
+                                <h4><del>৳{{ $product->product_price }}</del>&nbsp;&nbsp;&nbsp;&nbsp;৳{{
+                                    $discounted_price }}</h4>
+
+                                {{-- <ul class="color-variant">
+                                    <li class="bg-light0"></li>
+                                    <li class="bg-light1"></li>
+                                    <li class="bg-light2"></li>
+                                </ul> --}}
+                            </div>
+                        </div>
+                    </form>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endif
+@endif
+
+<!--=======================  New collection products start ==============================  -->
 @if($newProducts->count()> 0)
 <div class="title1 section-t-space">
     <h2 class="title-inner1">New collection</h2>
